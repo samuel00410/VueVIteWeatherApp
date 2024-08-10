2c3b510febae15260f3809f9381b15466908e3cc
samuel00410 <samuel00410@gmail.com>
2024-08-10 18:00:33 +0800

    製作跳出提示框 + Transition動畫

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -11,13 +11,35 @@
       </RouterLink>
       <div class="flex gap-3 flex-1 justify-end">
         <i
+          @click="toggleModal"
           class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
         ></i>
       </div>
+
+      <!-- 提示框 -->
+      <BaseModal :showModal="showModal" @close-modal="toggleModal">
+        <div class="text-black">
+          <h1 class="text-2xl mb-1">關於這個網站:</h1>
+          <p class="mb-4">搜尋你想選擇的城市的天氣</p>
+          <h2 class="text-2xl">它如何運作的:</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>在搜尋引擎輸入你想要查詢的城市名稱</li>
+            <li>選擇你輸入的搜尋結果(城市名稱)</li>
+          </ol>
+        </div>
+      </BaseModal>
     </nav>
   </header>
 </template>
 
 <script setup>
 import { RouterLink } from "vue-router";
+import BaseModal from "./BaseModal.vue";
+
+import { ref } from "vue";
+const showModal = ref(false);
+
+const toggleModal = () => {
+  showModal.value = !showModal.value;
+};
 </script>
