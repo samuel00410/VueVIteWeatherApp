614455d0aae6750cc50945f1148c5fec5b61aa72
LAPTOP-CO102DB4\User <samuel00410@gmail.com>
2024-08-15 23:26:43 +0800

    處理搜尋錯誤問題

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,23 @@
   <main class="container mx-auto text-white">
     <div class="pt-4 mb-8 relative">
       <input v-model="searchQuery" @input="getSearchResults" class="px-1 py-2 w-full bg-transparent border-b focus:outline-none focus:bg-weather-secondary focus:shadow-[0px_1px_0_0_#004E71]" type="text">
-      <ul v-if="mapboxSearchResults" class="absolute w-full bg-weather-secondary text-white shadow-md py-2 px-1 top-[66px]"> 
-        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
-          {{ searchResult.place_name }}
-        </li>
-      </ul> 
+        <ul v-if="mapboxSearchResults" class="absolute w-full bg-weather-secondary text-white shadow-md py-2 px-1 top-[66px]"> 
+          <p v-if="searchError">
+            搜尋出現問題，請稍後再試
+          </p>
+          <p v-if="!searchError && mapboxSearchResults.length === 0">
+            找不到相關地點，請重新輸入
+          </p>
+          <template v-else>
+            <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+              {{ searchResult.place_name }}
+            </li>
+          </template>
+        </ul> 
     </div>
   </main>
 </template>
- 
+  
 
 <script setup>
 import {ref} from "vue";
@@ -19,6 +27,7 @@ import axios from "axios";
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
 
 const mapboxAPIKey = import.meta.env.VITE_API_KEY
 
@@ -26,9 +35,13 @@ const getSearchResults = ()=>{
   clearTimeout(queryTimeout);
   queryTimeout.value = setTimeout(async()=>{
     if(searchQuery.value !== ""){
+      try{
         const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}`)
         mapboxSearchResults.value = result.data.features;
-        console.log(mapboxSearchResults.value)
+      }catch(e){
+        searchError.value = true;
+      }
+
         return;
     }
     
